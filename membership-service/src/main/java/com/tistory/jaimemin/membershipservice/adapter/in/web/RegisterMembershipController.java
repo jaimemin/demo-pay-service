b93434a0aeb49d956cf87b74662d7f533b14c6d7
jaimemin <jaimemin@gmail.com>
2024-12-04 22:17:55 +0900

    implemented sample test code for RegisterMembershipController

--- membership-service/src/main/java/com/tistory/jaimemin/membershipservice/adapter/in/web/RegisterMembershipController.java
+++ membership-service/src/main/java/com/tistory/jaimemin/membershipservice/adapter/in/web/RegisterMembershipController.java
@@ -10,7 +10,9 @@ import com.tistory.jaimemin.membershipservice.domain.Membership;
 
 import common.WebAdapter;
 import lombok.RequiredArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
 
+@Slf4j
 @WebAdapter
 @RestController
 @RequiredArgsConstructor
